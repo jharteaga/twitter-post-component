e8a508fac5880fb3effeb7534da1613af12d50c4
jharteaga <jh.arteaga09@gmail.com>
2020-04-24 15:38:28 -0700

    Updates

---
+++ precache-manifest.b3dab5dde15a96a8b111dc6f73e2d03c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6123841cf8d4ffee1bd4dad5dede65cd",
+    "url": "/twitter-post-component/index.html"
+  },
+  {
+    "revision": "25ccc188d043867971a0",
+    "url": "/twitter-post-component/static/css/2.1912a8e6.chunk.css"
+  },
+  {
+    "revision": "d69a456f05b04d140d26",
+    "url": "/twitter-post-component/static/css/main.ef90f44e.chunk.css"
+  },
+  {
+    "revision": "25ccc188d043867971a0",
+    "url": "/twitter-post-component/static/js/2.1dc95ee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twitter-post-component/static/js/2.1dc95ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69a456f05b04d140d26",
+    "url": "/twitter-post-component/static/js/main.65825e48.chunk.js"
+  },
+  {
+    "revision": "21e47895ffa194e690b6",
+    "url": "/twitter-post-component/static/js/runtime-main.6250f2cc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/twitter-post-component/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/twitter-post-component/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/twitter-post-component/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/twitter-post-component/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/twitter-post-component/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76af6665d4d01f511761d8e660c5e262.js"
+  "/twitter-post-component/precache-manifest.b3dab5dde15a96a8b111dc6f73e2d03c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/twitter-post-component/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
